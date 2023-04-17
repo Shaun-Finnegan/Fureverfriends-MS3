a5c74b4025b12d44f5ba615c0475840a09a7b8aa
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-04-17 14:28:38 +0000

    can add and read all the information from first model but not the second. All templating is done just need to complete C.R.U.D functionality

--- fureverfriends/routes.py
+++ fureverfriends/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from fureverfriends import app, db
-from fureverfriends.models import Customer
+from fureverfriends.models import Customer, Animal
 from sqlalchemy import text
 
 
@@ -32,7 +32,8 @@ def login():
 @app.route("/checklist", methods=["GET", "POST"])
 def checklist():
     customers = Customer.query.all()
-    return render_template("checklist.html", customers=customers)
+    animals = Animal.query.all()
+    return render_template("checklist.html", customers=customers, animals=animals)
 
 
 @app.route("/success", methods=["GET", "POST"])
@@ -44,22 +45,45 @@ def success():
             email=request.form.get('email'),
         )
 
-        db.session.add(customer)
+        animal = Animal(
+            animal=request.form.get('animal'),
+            animal_name=request.form.get('animal_name'),
+          
+        )
+
+        db.session.add(customer, animal)
         db.session.commit()
-    return render_template("index.html")
+    return render_template("success.html")
+
+
+
 
 
 @app.route("/edit/<int:id>",  methods=["GET", "POST"])
 def edit(id):
     customer = Customer.query.get_or_404(id)
+ 
     if request.method == 'POST':
         customer.first_name = request.form.get('fname'),
         customer.last_name = request.form.get('lname'),
         customer.email = request.form.get('email'),
+     
+        db.session.commit()
+        return redirect(url_for("checklist"))  
+    return render_template("edit.html", customer=customer)
+
+
+@app.route("/edit_animal/<int:id>",  methods=["GET", "POST"])
+def edit_animal(id):
+    animal = Animal.query.get_or_404(id)
+ 
+    if request.method == 'POST':
+        animal.animal = request.form.get('animal'),
+        customer.animal_name = request.form.get('animal_name'),
         
         db.session.commit()
-        return redirect(url_for("checklist"))   
-    return render_template("edit.html", customer=customer)
+        return redirect(url_for("checklist"))  
+    return render_template("edit.html", animal=animal)
 
 
 @app.route("/delete/<int:id>")
@@ -68,3 +92,12 @@ def delete(id):
     db.session.delete(customer)
     db.session.commit()
     return redirect(url_for("checklist"))
+
+
+@app.route("/delete_animal/<int:id>")
+def delete_animal(id):
+    animal = Animal.query.get_or_404(id)
+    db.session.delete(animal)
+    db.session.commit()
+    return redirect(url_for("checklist"))
+
